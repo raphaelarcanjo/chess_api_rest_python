392b3b6e4a7f20fba2a73b347c1a35f5750603fd
Raphael Arcanjo <raphael_arcanjo@live.com>
2022-03-30 19:55:26 -0300

    Some front-end structure

--- chess/views.py
+++ chess/views.py
@@ -7,3 +7,11 @@ def home(request):
         }
 
     return render(request, 'home.html', data)
+
+
+def about(request):
+    data = {
+        'title': "About"
+        }
+
+    return render(request, 'about.html', data)
